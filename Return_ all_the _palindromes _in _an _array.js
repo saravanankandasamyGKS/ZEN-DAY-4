abfd19a76e17ed91ead280800ff5cecd1254e35f
saravanankandasamyGKS <147834306+saravanankandasamyGKS@users.noreply.github.com>
2023-10-24 17:44:20 +0530

    IIFE Function
    IIFE Function

---
+++ Return_ all_the _palindromes _in _an _array.js
@@ -0,0 +1,12 @@
+(function() {
+    function isPalindrome(str) {
+      const reversed = str.split('').reverse().join('');
+      return str === reversed;
+    }
+  
+    const words = ["radar", "hello", "civic", "world"];
+    const palindromes = words.filter(function(word) {
+      return isPalindrome(word);
+    });
+    console.log(palindromes);
+  })();
