5eea4254155bb01578b745a59c2e69e880ac8a90
saravanankandasamyGKS <147834306+saravanankandasamyGKS@users.noreply.github.com>
2023-10-24 17:38:11 +0530

    anonymous function
     anonymous function

---
+++ Return_ all_ the_ prime _numbers _in _an _array.js
@@ -0,0 +1,15 @@
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+    for (let i = 5; i * i <= num; i += 6) {
+      if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+    return true;
+  }
+  
+  const numbers = [2, 3, 5, 7, 9, 11, 15];
+  const primeNumbers = numbers.filter(function(num) {
+    return isPrime(num);
+  });
+  console.log(primeNumbers);
